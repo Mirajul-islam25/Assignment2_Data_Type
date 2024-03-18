780531da0317e70d94297a70f52cb0194e405f89
Mirajul <islammirajul150978@gmail.com>
2024-03-18 19:13:53 +0600

    problemE12

---
+++ src/WithoutAComma.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class WithoutAComma {
+    public static void main(String[] args) {
+     Scanner Scanner = new Scanner(System.in);
+        System.out.printf("Enter any number with comma: ");
+        String inputNumber = Scanner.next();
+        int len = inputNumber.length();
+        String firstsome = inputNumber.substring(0,len-4);
+        String lastThree = inputNumber.substring(len-3);
+        System.out.println(firstsome + lastThree);
+    }
+}
